459e288f1dcbf04d82dc82d9a8d1155abda83641
DevFarid <faridkamizi@gmail.com>
2024-04-22 16:57:58 -0400

    test(ServerTests): add test cases, and fix junit 5

--- src/test/java/hive/ServerTests.java
+++ src/test/java/hive/ServerTests.java
@@ -1,22 +1,19 @@
-package tests.hive;
+package hive;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 
-import hive.HiveClient;
-import hive.Server;
-
 import org.javatuples.Pair;
 import org.junit.jupiter.api.*;
-import java.io.IOException;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicReference;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class HiveTests {
+public class ServerTests {
 
     /**
      * Setup a server on a given port.
@@ -40,7 +37,7 @@ public class HiveTests {
     }
 
     /**
-     * Setup a client on a given port.
+     * Set up a client on a given port.
      * @param port the port to connect the client to.
      * @return a <@code ListenableFuture> listener that will return true when the client is done connecting in the background.
      */
@@ -64,15 +61,14 @@ public class HiveTests {
 
     /**
      * Test if the server is up and running.
-     * @throws IOException
      */
     @Test
     @Order(1)
-    void testServerUp() throws IOException {
+    void testServerUp() {
         Pair<AtomicReference<Server>, ListenableFuture<Boolean>> server = setupServer(8080);
         server.getValue1().addListener(() -> {
             try {
-                Assertions.assertTrue((server.getValue0() == null || server.getValue0().get() == null) ? false : server.getValue0().get().isRunning());
+                Assertions.assertTrue(server.getValue0() != null && server.getValue0().get() != null && server.getValue0().get().isRunning());
                 server.getValue0().get().stop();
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -82,16 +78,15 @@ public class HiveTests {
 
     /**
      * Test if the client is initialized but not connected.
-     * @throws IOException
      */
     @Test
     @Order(2)
-    void testClientInitButNotConnected() throws IOException {
+    void testClientInitButNotConnected() {
         Pair<AtomicReference<HiveClient>, ListenableFuture<Boolean>> client = setupClient(8080);
         client.getValue1().addListener(() -> {
             try {
-                Assertions.assertTrue((client.getValue0() == null || client.getValue0().get() == null) ? false : client.getValue0().get().isRunning());
-                Assertions.assertTrue((client.getValue0() == null || client.getValue0().get() == null) ? false : !client.getValue0().get().isOnline());
+                Assertions.assertTrue(client.getValue0() != null && client.getValue0().get() != null && client.getValue0().get().isRunning());
+                Assertions.assertTrue(client.getValue0() != null && client.getValue0().get() != null && !client.getValue0().get().isOnline());
                 client.getValue0().get().stop();
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -101,32 +96,29 @@ public class HiveTests {
 
     /**
      * Test if the client is connected to the server.
-     * @throws IOException
      */
     @Test
     @Order(3)
-    void testClientConnectToServer() throws IOException {
+    void testClientConnectToServer() {
         Pair<AtomicReference<Server>, ListenableFuture<Boolean>> server = setupServer(8080);
         Pair<AtomicReference<HiveClient>, ListenableFuture<Boolean>> client = setupClient(8080);
 
         // wait for server.
         server.getValue1().addListener(()->{
             // wait for client.
-            client.getValue1().addListener(()-> {
-                server.getValue0().get().getConnectedClients().forEach(clientChannel -> {
-                    try {
-                        Assertions.assertTrue(
-                            client.getValue0().get().getChannel().isConnected() && 
-                            server.getValue0().get().getConnectedClients().contains(client.getValue0().get().getChannel())
-                        );
-                        
-                        client.getValue0().get().stop();
-                        server.getValue0().get().stop();
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }, Executors.newSingleThreadExecutor());
+            client.getValue1().addListener(()-> server.getValue0().get().getConnectedClients().forEach(clientChannel -> {
+                try {
+                    Assertions.assertTrue(
+                        client.getValue0().get().getChannel().isConnected() &&
+                        server.getValue0().get().getConnectedClients().contains(client.getValue0().get().getChannel())
+                    );
+
+                    client.getValue0().get().stop();
+                    server.getValue0().get().stop();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }), Executors.newSingleThreadExecutor());
 
         }, Executors.newSingleThreadExecutor());
     }
@@ -141,36 +133,25 @@ public class HiveTests {
         Pair<AtomicReference<HiveClient>, ListenableFuture<Boolean>> clientOne = setupClient(8080);
         Pair<AtomicReference<HiveClient>, ListenableFuture<Boolean>> clientTwo = setupClient(8080);
 
-        server.getValue1().addListener(()->{
-
-            clientOne.getValue1().addListener(()->{
-
-                clientTwo.getValue1().addListener(()->{
-
-                    server.getValue0().get().getConnectedClients().forEach(clientChannel -> {
-                        try {
-                            Assertions.assertTrue(
-                                clientOne.getValue0().get().getChannel().isConnected() && 
-                                server.getValue0().get().getConnectedClients().contains(clientOne.getValue0().get().getChannel())
-                            );
-
-                            Assertions.assertTrue(
-                                clientTwo.getValue0().get().getChannel().isConnected() && 
-                                server.getValue0().get().getConnectedClients().contains(clientTwo.getValue0().get().getChannel())
-                            );
-
-                            clientOne.getValue0().get().stop();
-                            clientTwo.getValue0().get().stop();
-                            server.getValue0().get().stop();
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
-
-                }, Executors.newSingleThreadExecutor());
-
-            }, Executors.newSingleThreadExecutor());
-
-        }, Executors.newSingleThreadExecutor());
+        server.getValue1().addListener(
+                ()-> clientOne.getValue1().addListener(
+                        () -> clientTwo.getValue1().addListener(
+                                ()-> server.getValue0().get().getConnectedClients().forEach(clientChannel -> {
+                                    try {
+                                        Assertions.assertTrue(
+                                                clientOne.getValue0().get().getChannel().isConnected() &&
+                                                        server.getValue0().get().getConnectedClients().contains(clientOne.getValue0().get().getChannel())
+                                        );
+                                        Assertions.assertTrue(
+                                                clientTwo.getValue0().get().getChannel().isConnected() &&
+                                                        server.getValue0().get().getConnectedClients().contains(clientTwo.getValue0().get().getChannel())
+                                        );
+                                        clientOne.getValue0().get().stop();
+                                        clientTwo.getValue0().get().stop();
+                                        server.getValue0().get().stop();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }), Executors.newSingleThreadExecutor()), Executors.newSingleThreadExecutor()), Executors.newSingleThreadExecutor());
     }
 }
