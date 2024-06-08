e102629d99d9ae2d9da239521395edf8e658b1eb
DevFarid <faridkamizi@gmail.com>
2024-06-08 01:18:09 -0400

    bump: version 0.3.6 → 0.3.7

--- src/main/java/hive/database/AppRepository.java
+++ src/main/java/hive/database/AppRepository.java
@@ -2,7 +2,14 @@ package hive.database;
 
 import com.j256.ormlite.support.ConnectionSource;
 import hive.packets.DBPacket;
-import hive.packets.Packet;
+import misc.ReflectionUtil;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This class will manage entities within the database in a CRUD manner.
@@ -10,10 +17,23 @@ import hive.packets.Packet;
  * Created by SixEyes on 2024-06-01.
  */
 public class AppRepository {
+    private final Logger logger = Logger.getLogger(AppRepository.class.getName());
     private final ConnectionSource connectionSource;
 
     public AppRepository(ConnectionSource connectionSource) {
         this.connectionSource = connectionSource;
+        try {
+            this.initializeEntities();
+        } catch (URISyntaxException e) {
+            this.logger.log(Level.SEVERE, "Error initializing entities.", e);
+        }
+    }
+
+    private void initializeEntities() throws URISyntaxException {
+        String entitiesFolder = String.format("%s/src/main/java/hive/database/entities", Paths.get("").toAbsolutePath());
+        File entitiesDirectory = new File(entitiesFolder);
+        List<File> entityFiles = ReflectionUtil.findJavaFiles(entitiesDirectory);
+
     }
 
     /**
--- src/main/java/hive/database/entities/TestEntity.java
+++ src/main/java/hive/database/entities/TestEntity.java
@@ -1,2 +1,17 @@
-package hive.database.entities;public class TestEntity {
+package hive.database.entities;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.UUID;
+
+@DatabaseTable(tableName = "TestEntity")
+public class TestEntity {
+
+    @DatabaseField(columnName = "id", generatedId = true, canBeNull = false, unique = true)
+    public UUID id;
+
+    public TestEntity() {
+        this.id = UUID.randomUUID();
+    }
 }
